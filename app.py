a381452077bb3548be3319460e722371356aa6b9
Moiz <moizsid69@gmail.com>
2023-03-19 15:27:05 +0530

    final chanegs

--- app.py
+++ app.py
@@ -1,14 +1,2 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return render_template('index.html')
-    #return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
-
 "Some changes added" 
-"some changes added" 
+ 
