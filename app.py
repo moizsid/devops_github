0745a7284ac4a5f47d925087bd065e8f4e1c7531
Moiz <moizsid69@gmail.com>
2023-03-19 15:23:52 +0530

    updated app.py code

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-"This is a pyhton app" 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+    #return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
+
+"Some changes added" 
+"some changes added" 
